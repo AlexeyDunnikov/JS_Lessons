f9c0434f64a428f8087946bd13a0579625671be1
”Dunnikov <dynnikovleha@gmail.com>
2021-11-25 13:49:22 +0300

    in progress

---
+++ Promises_2/js/index.js
@@ -0,0 +1,35 @@
+console.log("Request data...");
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Preparing data...");
+
+    const backendData = {
+      server: "aws",
+      port: 2000,
+      status: "working",
+    };
+
+    resolve(backendData);
+  }, 2000);
+});
+
+promise
+  .then((data) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        data.modified = true;
+        reject(new Error("Неизвестная ошибка"));
+        resolve(data);
+      }, 2000);
+    });
+  })
+  .then((clientData) => {
+    console.log("Data received", clientData);
+    clientData.fromPromise = true;
+    return clientData;
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.error("Error: ", err));
