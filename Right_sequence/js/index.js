bb6632b4a94f37c1d34756d0859ba1c2cd5b4183
”Dunnikov <dynnikovleha@gmail.com>
2021-12-03 12:28:09 +0300

    added valid function

---
+++ Right_sequence/js/index.js
@@ -0,0 +1,36 @@
+const s1 = '(gfdga)' //true
+const s2 = '(adfg)[gaf]{agf}' //true
+const s3 = '(aga]' //false
+const s4 = '{afg[agf]1231}' //true
+const s5 = '(hdfs[dfh}' //false
+const s6 = '(gda]{}' //false
+
+const openBrackets = ['{', '[', '('];
+const closeBrackets = ['}', ']', ')'];
+
+function isValid(s){
+    let stack = [];
+
+    for (let i = 0; i < s.length; i++){
+        if (openBrackets.indexOf(s[i]) !== -1){
+            stack.push(s[i])
+        }
+        else if (closeBrackets.indexOf(s[i]) !== -1){
+            if (
+              closeBrackets.indexOf(s[i]) != openBrackets.indexOf(stack.pop())
+            ) {
+              return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+console.log(isValid(s1));
+console.log(isValid(s2));
+console.log(isValid(s3));
+console.log(isValid(s4));
+console.log(isValid(s5));
+console.log(isValid(s6));
+
