635da4c2bdf1669c8576346a68d5dbc50997b121
”Dunnikov <dynnikovleha@gmail.com>
2021-12-10 11:09:49 +0300

    save

--- Online-cinema/js/index.js
+++ Online-cinema/js/index.js
@@ -2,9 +2,6 @@ import { slideMenu } from "./menu.js";
 import { renderVideo } from "./render_video.js";
 import { menuLinks } from "./menu_links.js";
 
-const MOVIE = "movie";
-const TV = "tv";
-
 slideMenu({
   menu: ".navigation",
   classActiveMenu: "navigation_active",
